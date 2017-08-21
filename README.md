54a5535479f9168320cc20e7731eedfa56754bd2
Sanders <dih765@dca90475f1cd.cof.ds.capitalone.com>
2017-08-21 16:19:33 -0400

    Created welcome screen, began game logic.

---
+++ README.md
@@ -0,0 +1,1 @@
+Trello: https://trello.com/b/FekhhvdO/unit-1-project
--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log('Ready to set sail');
+(function() {
+  console.log('Ready to play');
+  var score = 100;
+
+
+  $('.start img').on('click', function() {
+    console.log('start clicked');
+    $('html').css('background-image', `url('assets/game.jpg')`);
+    $('.welcome').hide();
+    createGame();
+  })
+
+  function createGame() {
+    clock();
+    $('#score span').text(score);
+
+    for (var i = 0; i < 3; i++) {
+      $('.container').append(`<img id ='target', '${i}' src='assets/target.png'>`);
+    }
+  } //END play()
+
+  $('#target').on('click', function() {
+    console.log('target hit');
+  });
+
+
+
+  //Playtime
+  function clock() {
+    var secs = 0;
+    var mins = 0;
+
+    timer = setInterval(function() {
+      secs++
+      if (secs < 10) {
+        secs = "0" + secs
+      } else if (secs == 60) {
+        secs = "00"
+        mins++
+        if (mins < 10) {
+          mins = "0" + mins
+        }
+      }
+      $('#timer').text(mins + ":" + secs)
+    }, 1000);
+  };
+  //end playtime
+
+})();
